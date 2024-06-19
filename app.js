8d4369a2565f2c59d7797d37fda8e5e3698f6667
ducanhRabbit <renyk97@gmail.com>
2024-06-19 18:06:10 +0700

    setting server

--- app.js
+++ app.js
@@ -4,14 +4,40 @@ const morgan = require('morgan')
 
 const rateLimit = require('express-rate-limit')
 
-const helmet = require('helmet')
+const app = express()
 
-const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 const mongoSanitize = require('express-mongo-sanitize')
 
-const app = express()
+const cors = require('cors')
 
-app.use(express.json({ limit: "10kb" }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(express.json({limit:'100kb'}))
+
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true
+}))
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:'true'}))
+
+app.use(helmet())
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Prevent DDOS attack
+const limiter = rateLimit({
+    max: 3000, // limite amount of request
+    windowMS: 60 * 60 * 1000, //block in 1 hour
+    message: 'Too many request from this IP'
+})
+
+module.exports = app
